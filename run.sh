aa102d4e47555c9be0e3a8c057acfc60941b9ac7
yliess86 <hatiyliess86@gmail.com>
2021-03-05 11:32:18 +0100

    Switch Program

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )" && SHADER=solar_clock python3 -m nps
+cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )" && python3 -m nps
